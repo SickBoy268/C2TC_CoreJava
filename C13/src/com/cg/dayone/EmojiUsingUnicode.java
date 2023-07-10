178cd118f0f721892ea8f0b62fda0f32b8071d08
Admin <Admin@DESKTOP-L06MLNQ>
2023-07-09 21:17:51 -0700

    First Commit

---
+++ C13/src/com/cg/dayone/EmojiUsingUnicode.java
@@ -0,0 +1,22 @@
+package com.cg.dayone;
+
+public class EmojiUsingUnicode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String a = "\u1F601";
+		System.out.println("Beaming Face With Smiling Eyes : " + a);
+		String b = "\u1F604";
+		System.out.println("Grinning Face With Smiling Eyes : " + b);
+		String c = "\u1F606";
+		System.out.println("Grinning Squinting Face : " + c);
+		String d = "\u1F600";
+		System.out.println("Grinning Face : " + d);
+		String e = "\u1F605";
+		System.out.println("Grinning Face With Sweat : " + e);
+		String f = "\u1F603";
+		System.out.println("Grinning Face With Big Eyes : " + f);
+
+	}
+
+}
